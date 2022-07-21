b795040d4f483903a776e2555391137e1cfcdc94
Maxim <cloudyfg@gmail.com>
2022-07-21 23:09:35 +0300

    DAO and repositories. Implemented pattern DAO.

---
+++ BankSystem/Services/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using BankSystem.Models;
+using System.Linq.Expressions;
+
+namespace BankSystem.Services.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        ExceptionModel Create(T item);
+        IEnumerable<T> Get();
+        T Get(Guid id);
+        T Get(Expression<Func<T, bool>> predicate);
+        ExceptionModel Update(T item);
+        ExceptionModel Delete(T item);
+        bool Exist(Guid id);
+        bool Exist(Expression<Func<T, bool>> predicate);
+    }
+}
